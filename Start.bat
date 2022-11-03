0d3777a9b3bf3fbd775e20b9c7aae30eb9cc6e4a
MostachoGG <117366707+MostachoGG@users.noreply.github.com>
2022-11-03 09:25:43 -0400

    Create Start.bat

---
+++ Start.bat
@@ -0,0 +1,30 @@
+@echo off
+:start
+::Name for CMD window
+set serverName=Jims DayZ server
+::Server files location
+set serverLocation="C:\Program Files (x86)\Steam\steamapps\common\DayZServer"
+::Server Port
+set serverPort=2302
+::Server config
+set serverConfig=serverDZ.cfg
+::Server profile folder
+set serverProfile=profileName
+::Logical CPU cores to use (Equal or less than available)
+set serverCPU=4
+::mods (@modName;@anotherModname;@mod)
+set mods=""
+::Sets title for terminal (DONT edit)
+title %serverName% batch
+::DayZServer location (DONT edit)
+cd "%serverLocation%"
+echo (%time%) %serverName% started.
+::Launch parameters (edit end: -config=|-port=|-profiles=|-doLogs|-adminLog|-netLog|-freezeCheck|-filePatching|-BEpath=|-cpuCount=)
+start "DayZ Server" /min "DayZServer_x64.exe" -config=%serverConfig% -mod=%mods% -port=%serverPort% -profiles=%serverProfile% -cpuCount=%serverCPU% -dologs -adminlog -netlog -freezecheck
+::Time in seconds before kill server process (14400 = 4 hours)
+timeout 14390
+taskkill /im DayZServer_x64.exe /F
+::Time in seconds to wait before..
+timeout 10
+::Go back to the top and repeat the whole cycle again
+goto start
